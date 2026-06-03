6877fc96a16ff2b4fa088ecaa91add372b373763
Radomir Stevanovic <radomir.stevanovic@gmail.com>
2026-06-03 15:54:37 -0700

    Refresh algorithms.markov module: fix docs style, add type hints

--- dwave/graphs/algorithms/markov.py
+++ dwave/graphs/algorithms/markov.py
@@ -12,10 +12,14 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-import dimod
+from collections.abc import Mapping, Sequence
 
-__all__ = ['sample_markov_network',
-           ]
+import networkx as nx
+
+import dimod
+from dimod.typing import Variable, Bias
+
+__all__ = ['sample_markov_network']
 
 
 ###############################################################################
@@ -50,35 +54,38 @@ __all__ = ['sample_markov_network',
 #
 
 
-def sample_markov_network(MN, sampler, fixed_variables=None,
-                          return_sampleset=False,
-                          **sampler_args):
-    """Samples from a markov network using the provided sampler.
+def sample_markov_network(MN: nx.Graph,
+                          sampler: dimod.Sampler,
+                          fixed_variables: Mapping[Variable, Bias] | None = None,
+                          return_sampleset: bool = False,
+                          **sampler_args,
+                          ) -> Sequence[Mapping[Variable, Bias]] | dimod.SampleSet:
+    """Samples from a Markov network using the provided sampler.
 
-    Parameters
-    ----------
-    MN : NetworkX graph
-        A Markov network as returned by :func:`~dwave.graphs.generators.markov.markov_network`
+    Args:
+        MN:
+            A Markov network as returned by
+            :func:`~dwave.graphs.generators.markov.markov_network`. Potentials
+            data is stored in a node/edge attribute ``potential``.
 
-    sampler : :class:`dimod.Sampler`
-        A dimod sampler.
+        sampler:
+            A dimod sampler.
 
-    fixed_variables : dict
-        A dictionary of variable assignments to be fixed in the markov network.
+        fixed_variables:
+            A dictionary of variable assignments to be fixed in the Markov
+            network.
 
-    return_sampleset : bool (optional, default=False)
-        If True, returns a :obj:`dimod.SampleSet` rather than a list of samples.
+        return_sampleset:
+            If True, returns a :class:`~dimod.SampleSet` rather than a list of
+            samples.
 
-    **sampler_args
-        Additional keyword parameters are passed to the sampler.
+        **sampler_args:
+            Additional keyword parameters are passed to the sampler.
 
-    Returns
-    -------
-    samples : list[dict]/:obj:`dimod.SampleSet`
+    Returns:
         A list of samples ordered from low-to-high energy or a sample set.
 
-    Examples
-    --------
+    Examples:
 
     >>> import dimod
     ...
@@ -120,11 +127,9 @@ def sample_markov_network(MN, sampler, fixed_variables=None,
     >>> samples[0]           # doctest: +SKIP
     {'a': 0, 'c': 0, 'b': 0}
 
-    Notes
-    -----
-    Samplers by their nature may not return the optimal solution. This
-    function does not attempt to confirm the quality of the returned
-    sample.
+    Note:
+        Samplers by their nature may not return the optimal solution. This
+        function does not attempt to confirm the quality of the returned sample.
 
     """
 
