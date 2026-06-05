a12d95b8fd7f0d9b470ca857cecd0f7a4038d7b7
Radomir Stevanovic <radomir.stevanovic@gmail.com>
2026-06-05 14:09:14 -0700

    Refresh generators.tsp module: fix docs style, add type hints

--- dwave/graphs/algorithms/tsp.py
+++ dwave/graphs/algorithms/tsp.py
@@ -12,7 +12,10 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
+from collections.abc import Hashable, Iterable, Sequence
+
 import dimod
+import networkx as nx
 
 __all__ = ["traveling_salesperson",
            "traveling_salesman",
@@ -20,71 +23,72 @@ __all__ = ["traveling_salesperson",
            ]
 
 
-def traveling_salesperson(G, sampler, lagrange=None, weight='weight',
-                          start=None, **sampler_args):
+def traveling_salesperson(graph: nx.Graph,
+                          sampler: dimod.Sampler,
+                          lagrange: float | None = None,
+                          weight: str = 'weight',
+                          start: Hashable | None = None,
+                          **sampler_args,
+                          ) -> Sequence[Hashable]:
     """Returns an approximate minimum traveling salesperson route.
 
-    Defines a QUBO with ground states corresponding to the
-    minimum routes and uses the sampler to sample
-    from it.
+    Defines a binary quadratic model (:term:`BQM`) with ground states corresponding to
+    the minimum routes and uses the sampler to sample from it.
 
     A route is a cycle in the graph that reaches each node exactly once.
     A minimum route is a route with the smallest total edge weight.
 
-    Parameters
-    ----------
-    G : NetworkX graph
-        The graph on which to find a minimum traveling salesperson route.
-        This should be a complete graph with non-zero weights on every edge.
+    Args:
+        graph:
+            The graph on which to find a minimum traveling salesperson route.
+            This should be a complete graph with non-zero weights on every edge.
 
-    sampler : :class:`dimod.Sampler`
-        A dimod sampler.
+        sampler:
+            A dimod sampler.
 
-    lagrange : number, optional (default None)
-        Lagrange parameter to weight constraints (visit every city once)
-        versus objective (shortest distance route).
+        lagrange:
+            Lagrange parameter to weight constraints (visit every city once)
+            versus objective (shortest distance route). If not specified, it's
+            estimated based on graph size.
+            See :meth:`~dimod.generators.tsp.traveling_salesperson` BQM generator.
 
-    weight : optional (default 'weight')
-        The name of the edge attribute containing the weight.
+        weight:
+            The name of the edge attribute containing the weight.
 
-    start : node, optional
-        If provided, the route will begin at `start`.
+        start:
+            If provided, the route will begin at ``start``.
 
-    sampler_args :
-        Additional keyword parameters are passed to the sampler.
+        **sampler_args:
+            Additional keyword parameters passed to the sampler.
 
-    Returns
-    -------
-    route : list
-       List of nodes in order to be visited on a route
+    Returns:
+       List of nodes in order to be visited on a route.
 
-    Examples
-    --------
+    Example:
+        >>> import dimod
+        >>> import dwave.graphs
+        >>> import networkx
+        ...
+        >>> G = networkx.Graph()
+        >>> G.add_weighted_edges_from({(0, 1, .1), (0, 2, .5), (0, 3, .1),
+        ...                            (1, 2, .1), (1, 3, .5), (2, 3, .1)})
+        >>> dwave.graphs.traveling_salesperson(G, dimod.ExactSolver(), start=0) # doctest: +SKIP
+        [0, 1, 2, 3]
 
-    >>> import dimod
-    ...
-    >>> G = nx.Graph()
-    >>> G.add_weighted_edges_from({(0, 1, .1), (0, 2, .5), (0, 3, .1), (1, 2, .1),
-    ...                            (1, 3, .5), (2, 3, .1)})
-    >>> dwave.graphs.traveling_salesperson(G, dimod.ExactSolver(), start=0) # doctest: +SKIP
-    [0, 1, 2, 3]
-
-    Notes
-    -----
-    Samplers by their nature may not return the optimal solution. This
-    function does not attempt to confirm the quality of the returned
-    sample.
+    Note:
+        Samplers by their nature may not return the optimal solution. This
+        function does not attempt to confirm the quality of the returned sample.
 
     """
     # Get a QUBO representation of the problem
-    bqm = dimod.generators.traveling_salesperson(G, lagrange, weight)
+    bqm = dimod.generators.traveling_salesperson(graph, lagrange=lagrange, weight=weight)
 
     # use the sampler to find low energy states
     response = sampler.sample(bqm, **sampler_args)
 
     sample = response.first.sample
 
-    route = [None] * len(G)
+    route = [None] * len(graph)
     for (city, time), val in sample.items():
         if val:
             route[time] = city
@@ -100,26 +104,20 @@ def traveling_salesperson(G, sampler, lagrange=None, weight='weight',
 traveling_salesman = traveling_salesperson
 
 
-def is_hamiltonian_path(G, route):
+def is_hamiltonian_path(graph: nx.Graph, route: Iterable[Hashable]) -> bool:
     """Determines whether the given list forms a valid TSP route.
 
-    A travelling salesperson route must visit each city exactly once.
+    A traveling salesperson route must visit each city exactly once.
 
-    Parameters
-    ----------
-    G : NetworkX graph
+    Args:
+        graph:
+            The graph on which to check the route.
 
-        The graph on which to check the route.
+        route:
+            List of nodes in the order that they are visited.
 
-    route : list
-
-        List of nodes in the order that they are visited.
-
-    Returns
-    -------
-    is_valid : bool
-        True if route forms a valid travelling salesperson route.
-
+    Returns:
+        True if route forms a valid traveling salesperson route.
     """
 
-    return set(route) == set(G)
+    return set(route) == set(graph)
